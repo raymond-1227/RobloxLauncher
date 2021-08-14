610c860f0f3910a3187a7e17ca8a10f5c5cdd894
Raymond Hsu <40461364+raymond-1227@users.noreply.github.com>
2021-08-15 05:40:24 +0800

    Upload files

---
+++ RobloxWebLauncher/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RobloxWebLauncher
+//
+//  Created by Raymond Hsu on 2021/8/15.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}

9859ceae0c6eff0b081a8218ad835724b6ab7eaf
Raymond Hsu <40461364+raymond-1227@users.noreply.github.com>
2022-02-09 19:49:05 +0800

    UI Improvements

---
+++ RobloxLauncher/AppDelegate.swift
@@ -0,0 +1,35 @@
+//
+//  AppDelegate.swift
+//  RobloxLauncher
+//
+//  Created by Raymond Hsu on August 15th, 2021.
+//
+
+import Cocoa
+import AppKit
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+    func open(_ urls: [URL],
+    withApplicationAt applicationURL: URL,
+    configuration: NSWorkspace.OpenConfiguration,
+              completionHandler: ((NSRunningApplication?, Error?) -> Void)? = nil) {
+    }
+    
+    
+}
